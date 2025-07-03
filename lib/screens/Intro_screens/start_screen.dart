0561fb63d4ba7e836f3f103efe1437e92d534296
roychandu <cr717753@gmail.com>
2025-07-03 11:15:28 +0530

    add start screen

--- lib/screens/Intro_screens/start_screen.dart
+++ lib/screens/Intro_screens/start_screen.dart
@@ -1,10 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_list_provider/common_widgets/Custom_button.dart';
 
 class StartScreen extends StatelessWidget {
   const StartScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          foregroundColor: Colors.white,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.only(left: 20, right: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Container(
+                child: Column(
+                  children: [
+                    Text(
+                      'Welcome to UpTodo',
+                      style: TextStyle(
+                        fontSize: 32,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 50),
+                    Text(
+                      textAlign: TextAlign.center,
+                      'Please login to your account or create new account to continue',
+                      style: TextStyle(fontSize: 16, color: Colors.white),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                child: Column(
+                  children: [
+                    SizedBox(
+                      width: double.infinity,
+                      child: CustomelevatedButton(
+                        text: 'Login',
+                        onPressed: () {},
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    SizedBox(
+                      width: double.infinity,
+                      child: OutlinedButton(
+                        style: OutlinedButton.styleFrom(
+                          side: BorderSide(color: Color(0xff8875FF)),
+                        ),
+                        onPressed: () {},
+                        child: Text(
+                          'Create Account',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
