460df7feb3c11d1348f50f5e6be7dfbb33ad9278
roychandu <cr717753@gmail.com>
2025-07-09 22:09:10 +0530

    color change login

--- lib/screens/Index_screens/home_screen.dart
+++ lib/screens/Index_screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_list_provider/common_widgets/color_extension.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,6 +9,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int selected = 2;
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -42,22 +45,49 @@ class _HomeScreenState extends State<HomeScreen> {
           ],
         ),
         bottomNavigationBar: BottomNavigationBar(
-          currentIndex: 0,
+          backgroundColor: Color(0xff363636),
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: TColor.secondaryText,
+          unselectedItemColor: TColor.primaryText,
+          currentIndex: selected,
+          onTap: (index) {
+            // here use provider or valuenotifire
+            setState(() {
+              selected = index;
+            });
+          },
+
           items: [
             BottomNavigationBarItem(
-              icon: Image.asset('assets/index.png'),
+              icon: Image.asset(
+                'assets/index.png',
+                height: 24,
+                color: selected == 0 ? TColor.secondaryText : Colors.white,
+              ),
               label: "Index",
             ),
             BottomNavigationBarItem(
-              icon: Image.asset('assets/calendar.png'),
+              icon: Image.asset(
+                'assets/calendar.png',
+                height: 24,
+                color: selected == 1 ? TColor.secondaryText : Colors.white,
+              ),
               label: "Celender",
             ),
             BottomNavigationBarItem(
-              icon: Image.asset('assets/clock.png'),
+              icon: Image.asset(
+                'assets/clock.png',
+                height: 24,
+                color: selected == 2 ? TColor.secondaryText : Colors.white,
+              ),
               label: "Focuse",
             ),
             BottomNavigationBarItem(
-              icon: Image.asset('assets/profile_icon.png'),
+              icon: Image.asset(
+                'assets/profile_icon.png',
+                height: 24,
+                color: selected == 3 ? TColor.secondaryText : Colors.white,
+              ),
               label: "Profile",
             ),
           ],
