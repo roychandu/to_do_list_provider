49fb592dcfe32d29358555057cbe00f7beca7ee2
roychandu <cr717753@gmail.com>
2025-07-16 21:20:59 +0530

    add background color

--- lib/common_widgets/color_extension.dart
+++ lib/common_widgets/color_extension.dart
@@ -3,5 +3,6 @@ import 'package:flutter/material.dart';
 class TColor {
   static Color get primary => Color(0xff363636);
   static Color get primaryText => Colors.white;
+  static Color get primaryTextBackground => Color(0xff272727);
   static Color get secondaryText => Color(0xff8687E7);
 }
