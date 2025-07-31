f55317310174b6dd5d28fcabcf98b87832cfd7f6
roychandu <cr717753@gmail.com>
2025-08-01 04:09:51 +0530

    add datepicker and custom month and year section

--- lib/screens/Index_screens/Tabs_screens/celender_tab_screen.dart
+++ lib/screens/Index_screens/Tabs_screens/celender_tab_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:table_calendar/table_calendar.dart';
 import 'package:to_do_list_provider/common_widgets/color_extension.dart';
+import 'package:date_picker_timeline/date_picker_timeline.dart';
+import 'package:intl/intl.dart';
 
 class CelenderTabScreen extends StatefulWidget {
   final List<String> celenderTask;
@@ -11,14 +12,64 @@ class CelenderTabScreen extends StatefulWidget {
 }
 
 class _CelenderTabScreenState extends State<CelenderTabScreen> {
+  DateTime _selectedDate = DateTime.now();
+
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+    String monthYear = DateFormat(
+      'MMMM \n yyyy',
+    ).format(_selectedDate).toUpperCase();
+
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(title: Text('Calendar'), centerTitle: true),
         body: widget.celenderTask.isEmpty
-            ? Column(children: [Container(child: Text('data'))])
+            ? Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Icon(
+                        Icons.arrow_back_ios,
+                        size: 16,
+                        color: Colors.white,
+                      ),
+                      const SizedBox(width: 8),
+                      Text(
+                        monthYear,
+                        style: const TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const SizedBox(width: 8),
+                      const Icon(
+                        Icons.arrow_forward_ios,
+                        size: 16,
+                        color: Colors.white,
+                      ),
+                    ],
+                  ),
+                  Container(
+                    height: screenHeight * 0.1,
+                    decoration: BoxDecoration(
+                      color: TColor.primaryTextBackground,
+                    ),
+                    child: DatePicker(
+                      DateTime.now(),
+                      initialSelectedDate: DateTime.now(),
+                      selectedTextColor: Colors.white,
+
+                      onDateChange: (date) {
+                        setState(() {});
+                      },
+                    ),
+                  ),
+                ],
+              )
             : ListView.builder(
                 itemCount: widget.celenderTask.length,
                 itemBuilder: (context, index) {
